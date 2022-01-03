58aff2e147725aa829fc222cadb1cbfb7605e909
puchkareva <puchkareva@yandex.ru>
2022-01-03 12:05:04 +0300

    initial commit

---
+++ FinalWork2021/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinalWork2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача: Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел.
+            
+        }
+    }
+}
